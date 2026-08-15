d37a9b09ca006257b3f2a16cc95b64602a37f68c
Andy <79422371+MushroomStew01@users.noreply.github.com>
2026-08-15 10:58:25 -0400

    Wire mobile resilience and refresh API

--- src/lexus_hub/app.py
+++ src/lexus_hub/app.py
@@ -6,6 +6,8 @@ from .map_assets import MapAssetProxyMiddleware
 from .map_assets import router as map_assets_router
 from .mobile_enhancements import MobileEnhancementMiddleware
 from .mobile_enhancements import router as mobile_enhancements_router
+from .mobile_resilience import MobileResilienceMiddleware
+from .mobile_resilience import router as mobile_resilience_router
 from .pwa import router as pwa_router
 from .resilience import DashboardMapFallbackMiddleware
 from .resilience import router as resilience_router
@@ -17,6 +19,8 @@ app.include_router(pwa_router)
 app.include_router(resilience_router)
 app.include_router(map_assets_router)
 app.include_router(mobile_enhancements_router)
+app.include_router(mobile_resilience_router)
 app.add_middleware(MobileEnhancementMiddleware)
 app.add_middleware(DashboardMapFallbackMiddleware)
 app.add_middleware(MapAssetProxyMiddleware)
+app.add_middleware(MobileResilienceMiddleware)
